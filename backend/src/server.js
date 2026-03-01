4267e25094a057ec3584e31438f6b65d58c77649
RaqeebDev <raqeebauto@gmail.com>
2026-03-01 16:51:37 +0530

    routes done!

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/api/auth/signup", (req,res) =>{
-  res.send("signup endpoint");
-});
-
-app.get("/api/auth/login", (req,res) =>{
-  res.send("login endpoint");
-});
-
-app.get("/api/auth/logout", (req,res) =>{
-  res.send("logout endpoint");
-});
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
