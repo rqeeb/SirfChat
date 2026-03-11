c20ff44cddef35d3562962ee6d8f158333937c3f
RaqeebDev <raqeebauto@gmail.com>
2026-03-11 14:30:43 +0530

    safety commit: Imported controller to message route

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -7,10 +7,14 @@ export const getAllContacts = async (req, res) => {
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
-
     res.status(200).json(filteredUsers);
   } catch (error) {
     console.log("Error in get getAllContacts: ", error);
     res.status(500).json({ message: "Server error" });
   }
+};
+
+
+export const getMessagesByUserId = async (req,res) =>{
+
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllContacts } from "../controllers/message.controller.js";
+import { getAllContacts , getMessagesByUserId} from "../controllers/message.controller.js";
 import {protectRoute} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/contacts",protectRoute, getAllContacts);
 // router.get("/chats", getChatPartners);
-// router.get("/:id", getMessagesByUserId);
+router.get("/:id", getMessagesByUserId);
 
 // router.post("/send/:id", sendMessage);
 
