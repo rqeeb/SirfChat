bc468477d5dca18c5f821134228f97794cdf3a37
RaqeebDev <raqeebauto@gmail.com>
2026-03-18 07:37:46 +0530

    form: Name Input and store

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -14,17 +14,43 @@ const SignUpPage = () => {
 
   return (
     <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px] ">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row">
-            {/* form - left  */}
-            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30  ">
-            <div className="w-full max-w-md">
+            {/* FORM - LEFT SIDE */}
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+              <div className="w-full max-w-md">
+                
+                <div className="text-center mb-8">
+                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
+                  <h2 className="text-2xl font-bold text-slate-200 mb-2">
+                    Create Account
+                  </h2>
+                  <p className="text-slate-400">Sign up for a new account</p>
+                </div>
 
-            </div>
-            </div>
+                {/* FORM */}
+                <form onSubmit={handleSubmit} className="space-y-6">
+               
+                  <div>
+                    <label className="auth-input-label">Full Name</label>
+                    <div className="relative">
+                      <UserIcon className="auth-input-icon" />
 
+                      <input
+                        type="text"
+                        value={formData.fullName}
+                        onChange={(e) =>
+                          setFormData({ ...formData, fullName: e.target.value })
+                        }
+                        className="input"
+                        placeholder="John Doe"
+                      />
+                    </div>
+                  </div>
 
+                
+                
           </div>
         </BorderAnimatedContainer>
       </div>
