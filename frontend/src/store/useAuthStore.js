f5aa100b24d39b87edb6b0d65785c68d405570e8
RaqeebDev <raqeebauto@gmail.com>
2026-04-06 14:50:46 +0530

    imported IO

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
